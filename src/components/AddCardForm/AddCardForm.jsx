e0f238b68671ae0dc799638cdf02936ae47ec60a
Guilherme Cazaroto <gui.cazaroto@gmail.com>
2021-02-04 20:39:00 -0300

    feat: handle events

--- src/components/AddCardForm/AddCardForm.jsx
+++ src/components/AddCardForm/AddCardForm.jsx
@@ -2,11 +2,40 @@ import React from "react"
 import './AddCardForm.css'
 
 class AddCardForm extends React.Component {
+
+  constructor () {
+    super()
+    this.title = '';
+  }
+
+  handleChangeTitle (event) {
+    this.title = event.target.value
+  }
+
+  handleChangeBody (event) {
+    this.body = event.target.value
+  }
+
+  handleCard (event) {
+    event.preventDefault()
+    console.log(this.title)
+    console.log(this.body)
+  }
+
   render() {
     return (
-      <form className="note-form">
-        <input type="text" placeholder="Título" /> <br />
-        <textarea cols="30" rows="10" placeholder="Descrição"></textarea> <br />
+      <form className="note-form" onSubmit={this.handleCard.bind(this)}>
+        <input
+          onChange={this.handleChangeTitle.bind(this)}
+          type="text"
+          placeholder="Título"
+        />
+        <textarea
+          onChange={this.handleChangeBody.bind(this)}
+          cols="30"
+          rows="10"
+          placeholder="Descrição"
+        />
         <button type="submit">Cadastrar</button>
       </form>
     )
